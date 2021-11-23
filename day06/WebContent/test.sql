c0d5a00718a320196cc2baaaf08f9cb383fb033c
SeungTae Bang <tw02485@naver.com>
2021-11-23 20:59:20 +0900

    JSP day06

--- day06/WebContent/test.sql
+++ day06/WebContent/test.sql
@@ -5,5 +5,11 @@ create  table test(
 	b varchar(10)
 );
 insert into test values(10,'홍길동');
+insert into test values(20,'김철수');
+insert into test values(30,'김영희');
+insert into test values(40,'김따릉');
+insert into test values(50,'김하나');
+insert into test values(60,'김무개');
+insert into test values(70,'김설춘');
 select * from test;
 drop table test;
--- day06/src/day06/Test.java
+++ day06/src/day06/Test.java
@@ -1,21 +1,67 @@
 package day06;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		String driver="oracle.jdbc.driver.OracleDriver";
+		String url="jdbc:oracle:thin:@localhost:1521:xe";
+		String user="bang";
+		String password="1234";
+	
+//		System.out.println("찾고싶은 번호 입려:");
+		System.out.println("찾고싶은 이름 입력 : ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+//		String num = sc.next();
+		String sql = "select * from test where b like '%"+name+"%'";
 		/*
 		1. JDBC 드라이버를 JVM에 로드 
 		2. DB에 연결
 		3. DBMS에 데이터를 read, write == SQL문을 수행
 		4. DB연결 해제
 		 */
-		
+		Connection conn = null;
+		Statement stmt = null;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch (ClassNotFoundException e) {
+			Class.forName(driver);
+//			System.out.println("JDBC 드라이버 로드 성공!");
+			
+			conn=DriverManager.getConnection(url, user, password);
+//			System.out.println("오라클에 연결 성공!");
+			
+			stmt=conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			//이터레이터와 유사한 rs
+			int cnt = 0;
+			while(rs.next()) {
+				System.out.println(rs.getInt("a")+":"+rs.getString("b"));
+				cnt++;
+			}
+			if(cnt==0) {
+				System.out.println("없는 데이터입니다!");
+			}
+			rs.close(); 
+			
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally{
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-
 }
