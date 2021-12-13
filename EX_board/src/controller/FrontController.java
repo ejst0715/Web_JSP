a0d162e4a13dd0dfa575b5192572026f65c9d78e
SeungTae Bang <tw02485@naver.com>
2021-12-13 21:50:57 +0900

    Ex_board->MVC2로변경(FrontController)

---
+++ EX_board/src/controller/FrontController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+// 사용자의 모든 요청을 FC로 향하게해야함!!!
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDO(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDO(request,response);
+	}
+
+	private void actionDO(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 요청을 파악
+		String uri=request.getRequestURI();
+		System.out.println(uri);
+		String cp=request.getContextPath();
+		System.out.println(cp);
+		String command=uri.substring(cp.length());
+		System.out.println(command);
+	}
+}
