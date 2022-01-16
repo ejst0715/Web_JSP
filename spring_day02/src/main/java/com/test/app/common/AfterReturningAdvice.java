eecb50d2cbb69a42bbc4b103e805959a6a66f0fb
SeungTae Bang <tw02485@naver.com>
2022-01-17 04:39:47 +0900

    spring 프레임워크 이용 게시판(DS,VR) -> AOP(어드바이스)
    xml->어노테이션

--- spring_day02/src/main/java/com/test/app/common/AfterReturningAdvice.java
+++ spring_day02/src/main/java/com/test/app/common/AfterReturningAdvice.java
@@ -1,23 +1,31 @@
 package com.test.app.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 
 import com.test.app.board.BoardVO;
 import com.test.app.member.MemberVO;
 
+@Service
+@Aspect
 public class AfterReturningAdvice {
-	public void afterLog(JoinPoint jp, Object obj) { // 바인드 변수
-		String coreMethod = jp.getSignature().getName();
-		System.out.println("현재 수행중인" + coreMethod + "()이후에 출력됨");
-
-		if (obj instanceof MemberVO) {
-			MemberVO vo = (MemberVO) obj; // 다운 캐스팅
-			System.out.println("리턴괸 객체는" + vo + "입니다.");
-		}else if(obj instanceof BoardVO){
-			BoardVO vo = (BoardVO) obj; // 다운 캐스팅
-			System.out.println("리턴괸 객체는" + vo + "입니다.");
-		}else {
-			System.out.println("false나옴");
+		
+	@AfterReturning(pointcut="PointcutCommon.bPointcut()",returning="obj")
+	public void afterLog(JoinPoint jp,Object obj) { // "바인드 변수"☆
+		String coreMethod=jp.getSignature().getName();
+		System.out.println("현재 수행중인 "+coreMethod+"()이후에 출력됨");
+		if(obj instanceof MemberVO) {
+			MemberVO vo=(MemberVO)obj; // 다운 캐스팅
+			System.out.println("리턴된 객체는 "+vo+"입니다.");
+		}
+		else if(obj instanceof BoardVO) {
+			BoardVO vo=(BoardVO)obj;
+			System.out.println("리턴된 객체는 "+vo+"입니다.");
+		}
+		else {
+			System.out.println("instanceof체크 false나옴!");
 		}
 	}
 }
--- spring_day02/src/main/java/com/test/app/common/AfterThrowingAdvice.java
+++ spring_day02/src/main/java/com/test/app/common/AfterThrowingAdvice.java
@@ -1,19 +1,27 @@
 package com.test.app.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 
-public class AfterThrowingAdvice {
-	public void exceptionLog(JoinPoint jp, Exception excep) { 	//바인드 변수
-		String coreMethod = jp.getSignature().getName();
-		System.out.println("비즈니스 메서드 " + coreMethod+"()수행중에 문제,예외 발생시 출력");
-		System.out.println("예외메세지: " + excep.getMessage());
+@Service
+@Aspect
+public class AfterThrowingAdvice {	
+	@AfterThrowing(pointcut="PointcutCommon.aPointcut()",throwing="excep")
+	public void exceptionLog(JoinPoint jp,Exception excep) { // 바인드 변수
+		String coreMethod=jp.getSignature().getName();
+		System.out.println("비즈니스 메서드 "+coreMethod+"() 수행중에 문제,예외 발생시 출력");
+		System.out.println("예외메세지: "+excep.getMessage());
 		
 		if(excep instanceof IllegalArgumentException) {
-			System.out.println("사용하지 않는 매게변수입니다.");
-		}else if(excep instanceof NullPointerException) {
-			System.out.println("객체생성 실패");
-		}else {
-			System.out.println("파악되지않은 예외");
+			System.out.println("사용하지않는 매개변수입니다.");
+		}
+		else if(excep instanceof NullPointerException) {
+			System.out.println("객체생성 실패했습니다.");
+		}
+		else {
+			System.out.println("현재 예외는 파악되지않은 예외입니다! 분석이 필요합니다!");
 		}
 	}
 }
