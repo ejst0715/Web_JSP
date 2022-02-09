492cd54f0e751ca7a8d257dac4aa3dc52f4d722d
SeungTae Bang <tw02485@naver.com>
2022-02-09 23:47:21 +0900

    삭제

--- spring_day02/src/main/java/com/test/app/member/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.test.app.member;
-
-public interface MemberService {
-	public MemberVO selectOne(MemberVO vo);
-	public void insertMember(MemberVO vo);
-	public void updateMember(MemberVO vo);
-}
